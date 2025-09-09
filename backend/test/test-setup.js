50de122150e92b0107d4889418d1b9de006d7993
Isurusandaruwan697 <isurusandaruwan697@gmail.com>
2025-09-09 21:04:58 +0530

    added new db update

---
+++ backend/test/test-setup.js
@@ -0,0 +1,29 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+
+let mongoServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  
+  // Close existing connections and connect to in-memory DB
+  await mongoose.disconnect();
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
+
+afterEach(async () => {
+  // Clean up all collections after each test
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany();
+  }
+});
